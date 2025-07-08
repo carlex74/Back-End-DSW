c05cb9a1b024c25ffd23a6d0c12d3e6061c4a89c
NiconiKimg <pedemontenicolas2004@gmail.com>
2025-07-08 16:08:01 -0300

    Delete DTOs folder Bug DB Connection Fixed
    Co-authored-by: LucaTvl <LucaTvl@users.noreply.github.com>

--- src/models/student/dtos/index.ts
+++
@@ -1,3 +0,0 @@
-export { CreateStudentDto } from './create-student.dto.js';
-export { UpdateStudentDto } from './update-student.dto.js';
-export { StudentResponseDto } from './student-response.dto.js';
--- src/models/student/dtos/update-student.dto.ts
+++
@@ -1,7 +0,0 @@
-export class UpdateStudentDto {
-  name?: string;
-  surname?: string;
-  mail?: string;
-  profile_picture?: string;
-  courseIds?: string[];
-}
