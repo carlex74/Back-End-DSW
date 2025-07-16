89cb4b22d013f5e090c5933a21427384614ea2bb
carlex74 <r.icardogugliermino@gmail.com>
2025-07-16 18:57:23 -0300

    Comentarios menores

--- src/shared/db/baseEntity.entity.ts
+++ src/shared/db/baseEntity.entity.ts
@@ -1,6 +1,11 @@
 import { PrimaryKey, SerializedPrimaryKey } from '@mikro-orm/core';
 import { ObjectId } from '@mikro-orm/mongodb';
 
+/**
+ * BaseEntity class that serves as a base for all entities in the application
+ * 
+ * It contains common properties such as _id and id.
+ */
 export abstract class BaseEntity {
   @PrimaryKey()
   _id?: ObjectId = new ObjectId();
