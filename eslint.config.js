27f3105fba79033ade9393857af39e7c137ea107
NiconiKimg <pedemontenicolas2004@gmail.com>
2025-08-17 21:59:51 -0300

    chore: configure ESLint with flat config for TypeScript project

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    ignores: ['dist/', 'eslint.config.js'],
+  },
+];
